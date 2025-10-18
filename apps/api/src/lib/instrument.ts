176be16df7e494f2d545ccb83fe001d16fc307f2
Alain <alain00.alvarez@gmail.com>
2025-10-18 08:16:42 -0400

    chore: update sentry environment

--- apps/api/src/lib/instrument.ts
+++ apps/api/src/lib/instrument.ts
@@ -1,7 +1,8 @@
 import { init } from "@sentry/node";
 
 init({
-	dsn: process.env.SENTRY_DSN!,
-	sendDefaultPii: true,
-	// enabled: process.env.NODE_ENV === "production",
+  dsn: process.env.SENTRY_DSN!,
+  sendDefaultPii: true,
+  environment: process.env.NODE_ENV,
+  // enabled: process.env.NODE_ENV === "production",
 });
