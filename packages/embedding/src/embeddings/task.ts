e2fcfa4179ee91e6a3db7cb80c2687a15f790ecf
Alain <alain00.alvarez@gmail.com>
2025-10-30 12:16:43 -0400

    chore: adding task description to embedding

--- packages/embedding/src/embeddings/task.ts
+++ packages/embedding/src/embeddings/task.ts
@@ -1,17 +1,28 @@
 import { embed } from "ai";
+import { stripHtml } from "string-strip-html";
 import { EMBEDDING_MODEL, EMBEDDING_OPTIONS } from "../constants";
 
-export const generateTaskEmbedding = async ({ title }: { title: string }) => {
-	const value = [title].filter(Boolean).join("\n");
+export const generateTaskEmbedding = async ({
+  title,
+  description,
+}: {
+  title: string;
+  description?: string | null;
+}) => {
+  const cleanTitle = stripHtml(title).result;
+  const cleanDescription = description
+    ? stripHtml(description).result
+    : undefined;
+  const value = [cleanTitle, cleanDescription].filter(Boolean).join("\n");
 
-	const embedding = await embed({
-		model: EMBEDDING_MODEL,
-		value,
-		providerOptions: EMBEDDING_OPTIONS,
-	});
+  const embedding = await embed({
+    model: EMBEDDING_MODEL,
+    value,
+    providerOptions: EMBEDDING_OPTIONS,
+  });
 
-	return {
-		embedding: embedding.embedding,
-		model: EMBEDDING_MODEL,
-	};
+  return {
+    embedding: embedding.embedding,
+    model: EMBEDDING_MODEL,
+  };
 };
