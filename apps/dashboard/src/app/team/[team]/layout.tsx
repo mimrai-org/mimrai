a52f3fae173e6ac94bdf666d60b88ee8ee79678d
Alain <alain00.alvarez@gmail.com>
2025-12-15 16:11:47 -0500

    chore: fix teamSlug

--- apps/dashboard/src/app/team/[team]/layout.tsx
+++ apps/dashboard/src/app/team/[team]/layout.tsx
@@ -19,10 +19,6 @@ export default async function Layout({ children, params }: Props) {
 		return redirect("/sign-in");
 	}
 
-	if (!session.user.teamSlug) {
-		return redirect("/team");
-	}
-
 	// switch to the team in the URL
 	try {
 		if (session.user.teamSlug !== team) {
