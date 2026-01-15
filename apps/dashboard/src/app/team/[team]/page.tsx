a47bc261a7573d2968b83a28b45c63839ffa5be7
Alain <alain00.alvarez@gmail.com>
2026-01-14 20:20:34 -0500

    chore: adding prefetchs

--- apps/dashboard/src/app/team/[team]/page.tsx
+++ apps/dashboard/src/app/team/[team]/page.tsx
@@ -5,6 +5,8 @@ import { NavSearch } from "@/components/nav-search";
 import { ProjectsList } from "@/components/projects/list";
 import { TeamSwitcher } from "@/components/team-switcher";
 import { getSession } from "@/lib/get-session";
+import { trpc } from "@/utils/trpc";
+import { getQueryClient, HydrateClient } from "@/utils/trpc-server";
 
 type Props = {
 	searchParams: Promise<{
@@ -14,26 +16,44 @@ type Props = {
 
 export default async function Page({ searchParams }: Props) {
 	const session = await getSession();
+	const queryClient = getQueryClient();
+
+	queryClient.prefetchQuery(trpc.zen.queue.queryOptions());
+	queryClient.prefetchInfiniteQuery(
+		trpc.projects.get.infiniteQueryOptions({
+			pageSize: 20,
+			search: "",
+		}),
+	);
+	queryClient.prefetchQuery(
+		trpc.integrations.getByType.queryOptions({
+			type: "github",
+		}),
+	);
+	queryClient.prefetchQuery(trpc.teams.getAvailable.queryOptions());
+	queryClient.prefetchQuery(trpc.users.getCurrent.queryOptions(undefined));
 
 	return (
-		<div className="flex h-screen w-screen flex-col items-center justify-center pb-4">
-			<div className="max-w-4xl animate-blur-in space-y-8">
-				<div className="space-y-1">
-					<div className="w-fit">
-						<TeamSwitcher />
+		<HydrateClient>
+			<div className="flex h-screen w-screen flex-col items-center justify-center pb-4">
+				<div className="w-4xl animate-blur-in space-y-8">
+					<div className="space-y-1">
+						<div className="w-fit">
+							<TeamSwitcher />
+						</div>
+						<h1 className="flex items-center gap-2 font-header text-4xl">
+							Hello, {session?.user?.name.split(" ")[0]}
+						</h1>
+						<p className="text-muted-foreground text-sm">
+							All your projects in one place.
+						</p>
 					</div>
-					<h1 className="flex items-center gap-2 font-header text-4xl">
-						Hello, {session?.user?.name.split(" ")[0]}
-					</h1>
-					<p className="text-muted-foreground text-sm">
-						All your projects in one place.
-					</p>
+					<NavTopTasks />
+					<NavSearch />
+					<NavWorkspace />
+					<ProjectsList showFilters={false} />
 				</div>
-				<NavTopTasks />
-				<NavSearch />
-				<NavWorkspace />
-				<ProjectsList showFilters={false} />
 			</div>
-		</div>
+		</HydrateClient>
 	);
 }
