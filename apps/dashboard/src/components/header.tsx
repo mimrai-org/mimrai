fb73f643c1514e06b993d3820868c949e7032923
Alain <alain00.alvarez@gmail.com>
2026-01-24 11:07:02 -0500

    chore: styles

--- apps/dashboard/src/components/header.tsx
+++ apps/dashboard/src/components/header.tsx
@@ -7,18 +7,20 @@ import { StickySidebarClose } from "./sticky-sidebar";
 
 export default function Header() {
 	return (
-		<header className="container mx-auto h-[75px] px-4 sm:px-0">
-			<div className="flex h-full flex-col items-start justify-center py-4">
-				<div className="flex w-full items-center justify-between">
-					<div className="flex items-center gap-4">
-						<StickySidebarClose />
-						<Breadcrumbs />
-						<NavSearch />
-					</div>
-					<div className="flex items-center gap-4">
-						{/* <NavSuggestions /> */}
-						<div className="ml-2">
-							<NavUser />
+		<header className="mb-2 h-[55px]">
+			<div className="container mx-auto h-full px-4 sm:px-0">
+				<div className="flex h-full flex-col items-start justify-center py-4">
+					<div className="flex w-full items-center justify-between">
+						<div className="flex items-center gap-4">
+							<StickySidebarClose />
+							<Breadcrumbs />
+							<NavSearch />
+						</div>
+						<div className="flex items-center gap-4">
+							{/* <NavSuggestions /> */}
+							<div className="ml-2">
+								<NavUser />
+							</div>
 						</div>
 					</div>
 				</div>
