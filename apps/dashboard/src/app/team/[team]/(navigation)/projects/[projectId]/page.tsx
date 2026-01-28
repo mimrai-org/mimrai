929cba34388680dce85208d5709af006ca558d2b
Alain <alain00.alvarez@gmail.com>
2026-01-28 11:42:20 -0500

    chore: add project crumb

--- apps/dashboard/src/app/team/[team]/(navigation)/projects/[projectId]/page.tsx
+++ apps/dashboard/src/app/team/[team]/(navigation)/projects/[projectId]/page.tsx
@@ -1,3 +1,5 @@
+import { FolderOpen } from "lucide-react";
+import { BreadcrumbSetter } from "@/components/breadcrumbs";
 import { TasksView } from "@/components/tasks-view/tasks-view";
 import { trpcClient } from "@/utils/trpc";
 
@@ -8,12 +10,24 @@ type Props = {
 export default async function Page({ params }: Props) {
 	const { projectId, team } = await params;
 
+	const project = await trpcClient.projects.getById.query({
+		id: projectId,
+	});
+
 	const defaultView = await trpcClient.taskViews.getDefault.query({
 		projectId,
 	});
 
 	return (
 		<div className="h-full">
+			<BreadcrumbSetter
+				crumbs={[
+					{
+						label: project.name,
+						segments: ["projects", project.id],
+					},
+				]}
+			/>
 			<TasksView
 				defaultFilters={{
 					...defaultView?.filters,
